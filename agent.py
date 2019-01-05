7fb37bd13525c96ea1669f1043eb3fe31c20e70c
babar21 <33437101+babar21@users.noreply.github.com>
2019-01-05 12:09:19 +0100

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Agent class
+Everything needed for an agent to :
+    - get input from the environment
+    - choose an action according to a policy
+    
+"""
+
