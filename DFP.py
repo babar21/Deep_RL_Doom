4b23058ec4d8af2473832b2efb9076fbef6074c5
babar21 <33437101+babar21@users.noreply.github.com>
2019-01-07 15:49:01 +0100

    train methods added

--- DFP.py
+++ DFP.py
@@ -12,6 +12,8 @@ from keras.models import Model
 import numpy as np
 from utils_network import normalize_layer, get_optimizer
 from parsers import parse_image_params, parse_measure_params, parse_goal_params, parse_action_params, parse_expectation_params
+from replay_memory import ReplayMemory
+import cv2
 
 
 class DFP_agent(Agent):
@@ -26,7 +28,11 @@ class DFP_agent(Agent):
             dico_init_network
             dico_init_policy
         """
-        
+        self.
+        self.step_btw_train = 64 
+        self.decrease_eps = 'function to decrease eps'
+        self.max_size = dico_init_network['max_size']
+        self.nb_action = ''
         self.network = create_network(image_params, measure_params, goal_params, expectation_params, 
                                action_params, optimizer_params, leaky_param)
 
@@ -35,11 +41,12 @@ class DFP_agent(Agent):
         """
         Choose action according to the eps-greedy policy using the network for inference
         Inputs : 
-            eps
+            eps : eps parameter for the eps-greedy policy
+            goal : column vector encoding the goal for each timesteps and each measures
+            screen : raw input from the game
+            game_features : raw features from the game
         Returns an action coded by an integer
         """
-        input_screen, input_game_features = read_input_state(self, screen, game_features)
-        
         # eps-greedy policy used for exploration (if want full exploitation, just set eps to 0)
         if np.random.rand() < eps :
             action = np.random.randint(0,self.nb_action)
@@ -50,23 +57,91 @@ class DFP_agent(Agent):
             list_act = np.dot(pred_measure_calc,goal_calc)
             action = np.argmax(list_act)
         
-        return
-    
-    
-    def train(self):
-        """
-        Train the bot (ie the network) according to an eps-greedy policy
-        Use a replay memory (see dedicated class)
-        """
-        return
+        return action
     
     
     def read_input_state(self, screen, game_features):
         """
-        Use grey level image and several stacked images
+        Use grey level image and specific image definition
         """
+        input_screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+        input_screen = cv2.resize(input_screen, (self.image_size, self.image_size))
+        input_game_features = np.zeros(len(self.game_features))
+        i=0
+        for features in self.game_features :
+            input_game_features[i] = game_features[features]
+            i +=1
+        return input_screen, input_game_features
+    
+    
+    def train(self, experiment, nb_episodes):
+        """
+        Train the bot according to an eps-greedy policy
+        Use a replay memory (see dedicated class)
+        Inputs : 
+            experiment : object from the experiment class, which contains the game motor
+        """
+        # variables
+        last_states = []
+        nb_step = 0
         
-        return
+        # create game from experiment
+        experiment.start(map_id=map_id,
+                        episode_time=self.params.episode_time,
+                        log_events=False)
+        
+        # create replay memory
+        replay_mem = ReplayMemory(self.max_size, self.replay_memory['screen_shape'], self.replay_memory['n_variables'], self.replay_memory['n_features'])
+        
+        # run training
+        for episode in range(nb_episodes):
+            
+            # reset game for a new episode
+            experiment.reset()   
+            
+            while not experiment.is_final():
+                # get screen and features from the game 
+                screen, game_features = experiment.observe_state(params, last_states)
+                
+                # decrease eps according to a fixed policy
+                eps = self.decrease_eps(self,episode)
+                
+                # choose action
+                input_screen, input_game_features = self.read_input_state(screen, game_features)
+                action,  = self.act_opt(eps, input_screen, input_game_features, goal)
+                
+                # make action and observe resulting measurement (plays the role of the reward)
+                experiment.make_action(action, self.params.frame_skip)
+                screen, game_features = experiment.observe_state(params, last_states)
+                input_screen_next, input_game_features_next = self.read_input_state(screen, game_features)
+                
+                # save last processed screens / features / action in the replay memory
+                replay_mem.add( screen=input_screen,
+                                variables=None,
+                                features=input_game_features,
+                                action=action,
+                                reward=input_game_features_next,
+                                is_final=self.game.is_final()
+                            )
+                
+                # train network if needed
+                if nb_step%self.step_btw_train :
+                    self.train(replay_mem)
+                
+                
+                # count nb of step since start
+                nb_step += nb_step
+      
+        
+    def train_network(self, replay_memory):
+        """
+        train the network according to a batch size and a replay memory
+        """
+        train_set = replay_memory.get_batch(self.batch_size, 1)
+        
+        
+        
+        
     
     
     @staticmethod    
@@ -92,9 +167,8 @@ class DFP_agent(Agent):
         # Define optimizer
         optimizer = get_optimizer(optimizer_params)
         
-        
         # Image stream
-        screen_input = Input(shape=(screen_input_size, screen_input_size,1)) 
+        screen_input = Input(shape=screen_input_size) 
         s1 = Conv2D(s1['channel'], (s1['kernel'],s1['kernel']), strides=(s1['stride'], s1['stride']), activation='linear')(screen_input)
         s1 = LeakyReLU(alpha=leaky_param)(s1)
         s2 = Conv2D(s2['channel'], (s2['kernel'],s2['kernel']), strides=(s2['stride'], s2['stride']), activation='linear')(s1)
