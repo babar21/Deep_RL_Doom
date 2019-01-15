a61f1488cc5988f093e510dce5d02c566c37d229
babar21 <33437101+babar21@users.noreply.github.com>
2019-01-15 19:26:42 +0100

    Add files via upload

--- Bots/utils_network.py
+++ Bots/utils_network.py
@@ -28,23 +28,24 @@ def get_optimizer(optimizer_params):
             'type' -> optimizer type
             other -> optimizer legit parameters
     """
-    if 'type' in optimizer_params:
-        method = optimizer_params.pop('type', None)
+    optimizer_param = optimizer_params.copy()
+    if 'type' in optimizer_param:
+        method = optimizer_param.pop('type', None)
     else :
         raise Exception('You must provide an optimization method')
 
     if method == 'adadelta':
-        optim_fn = kopt.Adadelta(**optimizer_params)
+        optim_fn = kopt.Adadelta(**optimizer_param)
     elif method == 'adagrad':
-        optim_fn = kopt.Adagrad(**optimizer_params)
+        optim_fn = kopt.Adagrad(**optimizer_param)
     elif method == 'adam':
-        optim_fn = kopt.Adam(**optimizer_params)
+        optim_fn = kopt.Adam(**optimizer_param)
     elif method == 'adamax':
-        optim_fn = kopt.Adamax(**optimizer_params)
+        optim_fn = kopt.Adamax(**optimizer_param)
     elif method == 'sgd':
-        optim_fn = kopt.SGD(**optimizer_params)
+        optim_fn = kopt.SGD(**optimizer_param)
     elif method == 'rmsprop':
-        optim_fn = kopt.RMSprop(**optimizer_params)
+        optim_fn = kopt.RMSprop(**optimizer_param)
     else :
         raise Exception('Unknown optimization method: "%s"' % method)
 
@@ -52,13 +53,13 @@ def get_optimizer(optimizer_params):
 
 #%% saving
     
-def saving_stats(nb_episode, stats, network):
+def saving_stats(nb_episode, stats, network, name):
     """
     saving weights and stats from the experiment
     """
     # game stats
-    with open('game_stats_{}'.format(nb_episode),'wb') as fp:
+    with open('{}_game_stats_{}'.format(name,nb_episode),'wb') as fp:
         pickle.dump(stats,fp)
     
     # network
-    network.save('network_{}'.format(nb_episode))
+    network.save('{}_network_{}'.format(name,nb_episode))
